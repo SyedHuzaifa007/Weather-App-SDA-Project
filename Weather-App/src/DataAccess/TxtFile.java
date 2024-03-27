2a3c1122c34aeec019bcb704b99e69a2eee5dcff
AhmadShamailButt <ahmadshamailbutt@gmail.com>
2024-03-27 06:24:08 +0500

    Update TxtFile.java

--- Weather-App/src/DataAccess/TxtFile.java
+++ Weather-App/src/DataAccess/TxtFile.java
@@ -9,7 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
- class TxtFile {
+ public class TxtFile {
     private String location;
 
     public TxtFile(String loc) {
