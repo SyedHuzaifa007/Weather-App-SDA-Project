eed852065900b368106fa54cd1b07444eee61a79
AdolfTrumpler <116947935+AdolfTrumpler@users.noreply.github.com>
2024-04-28 19:06:33 +0500

    UI Database
    Fixed some issues and passed parameters to UI and made a menu for main

--- Weather-App/src/BusinessLogic/Main.java
+++ Weather-App/src/BusinessLogic/Main.java
@@ -2,6 +2,8 @@ package BusinessLogic;
 import DataAccess.DBTxtManager;
 import DataAccess.DatabaseSQL;
 import UI.GUI;
+import UI.UITerminalBased;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 
@@ -9,9 +11,6 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
-//-----------------------------------GUI Object------------------------------------------//
-        GUI G = new GUI();
-        G.createLoadingFrame();
 //-----------------------------------Location Object-------------------------------------//
         location location = new location();
         location.getCurrentLocation();
@@ -25,8 +24,22 @@ public class Main
         DBTxtManager hello = new DBTxtManager();
         hello.writeToDBTxt(db.get(0), db.get(1), db.get(2));
         DatabaseSQL.main(args);
+//-----------------------------------UI option-------------------------------------//
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press 1 to use GUI and 2 to use UI");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+//-----------------------------------GUI Object------------------------------------------//
+        GUI G = new GUI();
+        G.createLoadingFrame();
+
 //-----------------------------------GUI called------------------------------------------//
         G.add(data.get(0), data.get(1), data.get(2), data.get(3), data.get(4), data.get(5), data.get(6), data.get(7), data.get(8), data.get(9), data.get(10), data.get(11), data.get(12), data.get(13), data.get(14), data.get(15),
-                data.get(16), data.get(17), data.get(18), data.get(19), data.get(20), data.get(21), data.get(22), data.get(23), data.get(24), data.get(25));
+        data.get(16), data.get(17), data.get(18), data.get(19), data.get(20), data.get(21), data.get(22), data.get(23), data.get(24), data.get(25));}
+//-----------------------------------UI object and called------------------------------------------//
+        else if (choice==2){
+        UITerminalBased ui=new UITerminalBased();
+        ui.run(db,args,manager);}
+
     }
 }
