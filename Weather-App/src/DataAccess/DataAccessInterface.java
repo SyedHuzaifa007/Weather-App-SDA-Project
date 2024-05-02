35d173136742233099c6f070d4efacd8815d07f3
AhmadShamailButt <ahmadshamailbutt@gmail.com>
2024-05-02 10:32:56 +0500

    Revert "Update DataAccess"
    This reverts commit 286455945121f7c4b0a163ddcb05665e5150164c.

--- Weather-App/src/DataAccess/DataAccessInterface.java
+++ Weather-App/src/DataAccess/DataAccessInterface.java
@@ -8,5 +8,5 @@ public interface DataAccessInterface {
     public void insertWeatherData();
     public void insertWeatherForecast();
     public void insertAirPollutionData();
-    public void getData();
+    public void getdata();
 }
